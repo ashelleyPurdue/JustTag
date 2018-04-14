749467ad2fa285f001a458fa7a0f6e71a182ee08
Alex Shelley <ashelley@purdue.edu>
2018-04-14 16:51:49 -0400

    Add project files.

---
+++ FileBrowserWPF/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace FileBrowserWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NavigationStack<string> pathHistory;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pathHistory = new NavigationStack<string>(Directory.GetCurrentDirectory());
+            UpdateCurrentDirectory();
+        }
+        
+        // Misc methods
+
+        private void UpdateCurrentDirectory()
+        {
+            Console.WriteLine(pathHistory.ToString());
+
+            // Move to the directory
+            Directory.SetCurrentDirectory(pathHistory.Current);
+            DirectoryInfo currentDir = new DirectoryInfo(pathHistory.Current);
+
+            // Set the textbox to the working directory
+            currentPathBox.Text = pathHistory.Current;
+
+            // Update the forward/back buttons
+            forwardButton.IsEnabled = pathHistory.HasNext;
+            backButton.IsEnabled = pathHistory.HasPrev;
+
+            // Populate the list box
+            folderContentsBox.Items.Clear();
+            
+            var subdirs = currentDir.EnumerateDirectories();
+            foreach (DirectoryInfo dir in subdirs)
+                folderContentsBox.Items.Add(dir.Name);
+
+            var files = currentDir.EnumerateFiles();
+            foreach (FileInfo file in files)
+                folderContentsBox.Items.Add(file.Name);
+        }
+
+
+        // Event handlers
+
+        private void currentPathBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;     // Don't go on if it's not the enter key
+
+            // Navigate to the place.
+            if (Directory.Exists(currentPathBox.Text))
+            {
+                pathHistory.Push(currentPathBox.Text);
+                UpdateCurrentDirectory();
+            }
+        }
+
+        private void currentPathBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Change to a red background if the text is not a valid path
+
+            bool valid = Directory.Exists(currentPathBox.Text);
+            currentPathBox.Background = valid ? Brushes.White : Brushes.Red;
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            pathHistory.MoveBack();
+            UpdateCurrentDirectory();
+        }
+
+        private void forwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            pathHistory.MoveForward();
+            UpdateCurrentDirectory();
+        }
+    }
+}
---
+++ FileBrowserWPF/NavigationStack.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileBrowserWPF
+{
+    /// <summary>
+    /// A stack, except you can move backwards and fowards
+    /// If you push something after moving backwards, everything
+    /// after the position will be forgotten.
+    /// 
+    /// It's just like the forward/back buttons on your web browser.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class NavigationStack<T>
+    {
+        /// <summary>
+        /// Returns the item at the current position
+        /// </summary>
+        /// <returns></returns>
+        public T Current
+        {
+            get { return items[currentPos]; }
+        }
+
+        /// <summary>
+        /// True if MoveForward() will do anything
+        /// </summary>
+        public bool HasNext { get { return currentPos != items.Count - 1; } }
+
+        /// <summary>
+        /// True if MoveBack() will do anything
+        /// </summary>
+        public bool HasPrev { get { return currentPos != 0; } }
+
+        private List<T> items = new List<T>();
+        private int currentPos = 0;
+
+        public NavigationStack(T firstItem)
+        {
+            items.Add(firstItem);
+        }
+
+        /// <summary>
+        /// Returns the previous item and rewinds.
+        /// If there are no previous items, returns the current item
+        /// </summary>
+        /// <returns></returns>
+        public T MoveBack()
+        {
+            if (currentPos != 0)
+                currentPos--;
+
+            return items[currentPos];
+        }
+
+        /// <summary>
+        /// Returns the next item and fast-forwards
+        /// If there is no next item, returns the current item.
+        /// </summary>
+        /// <returns></returns>
+        public T MoveForward()
+        {
+            if (currentPos != items.Count - 1)
+                currentPos++;
+
+            return items[currentPos];
+        }
+
+        /// <summary>
+        /// Adds a new item after the current position
+        /// All items after the current position are forgotten,
+        /// like in a web browser
+        /// </summary>
+        /// <param name="item"></param>
+        public void Push(T item)
+        {
+            // Remove all items after currentPos
+            while (items.Count - 1 > currentPos)
+                items.RemoveAt(items.Count - 1);
+
+            // Add the new item
+            items.Add(item);
+            currentPos++;
+        }
+
+        /// <summary>
+        /// Prints all items
+        /// A carrot  is added to the line with the current position
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                // Add a carrot if this is the current pos
+                if (i == currentPos)
+                    builder.Append(">");
+
+                // Add the item
+                builder.AppendLine(items[i].ToString());
+            }
+
+            return builder.ToString();
+        }
+    }
+}
