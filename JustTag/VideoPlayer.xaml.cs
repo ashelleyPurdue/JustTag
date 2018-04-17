a2a79de769b829c7009add8a3ad2d0f9199c600e
Alex Shelley <ashelley@purdue.edu>
2018-04-17 15:34:42 -0400

    copied the event handlers for the slider and play button.

--- JustTag/VideoPlayer.xaml.cs
+++ JustTag/VideoPlayer.xaml.cs
@@ -24,5 +24,61 @@ namespace JustTag
         {
             InitializeComponent();
         }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Play/pause the video
+            PlayOrPause(!videoPlaying);
+        }
+
+        private void videoTimeSlider_MouseMoved(object sender, MouseEventArgs e)
+        {
+            // Don't do anything if the user isn't holding a button
+            if (e.LeftButton != MouseButtonState.Pressed)
+                return;
+
+            // Find the time to skip to
+            double percent = videoTimeSlider.Value / videoTimeSlider.Maximum;
+            double time = videoPlayer.NaturalDuration.TimeSpan.TotalSeconds * percent;
+
+            // Jump to the time
+            videoPlayer.Position = TimeSpan.FromSeconds(time);
+
+            // Show one frame
+            videoPlayer.Play();
+            System.Threading.Thread.Sleep(2);
+            videoPlayer.Pause();
+
+            // Don't let this event fire again until a minimum amount of time has passed
+            // This way we don't get a bunch of micro-jumps as the user moves.
+            videoTimeSlider.MouseMove -= videoTimeSlider_MouseMoved;
+            new System.Threading.Thread(() =>
+            {
+                // Sleep for a bit
+                System.Threading.Thread.Sleep(30);
+
+                // Resubscribe to the event
+                videoTimeSlider.MouseMove += videoTimeSlider_MouseMoved;
+            }).Start();
+
+        }
+
+        private void videoTimeSlider_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Don't let the slider move on its own while the user is dragging it
+            PlayOrPause(false);
+        }
+
+        private void SliderUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            // Don't do anything if the open file is not a video
+            if (!videoPlayer.NaturalDuration.HasTimeSpan)
+                return;
+
+            // Update the slider to match the video time
+            double percent = videoPlayer.Position.TotalSeconds / videoPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+
+            videoTimeSlider.Value = percent * videoTimeSlider.Maximum;
+        }
     }
 }
