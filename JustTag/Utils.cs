83b970a3284a132e46f4a12dbc20a0be5247c3ee
Alex Shelley <ashelley@purdue.edu>
2018-04-18 20:02:57 -0400

    Moved some methods off to Utils.cs.  Implemented the shuffling function.

---
+++ JustTag/Utils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using IWshRuntimeLibrary;
+
+namespace JustTag
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Follows a shortcut and returns the FileSystemInfo that it points to.
+        /// </summary>
+        /// <param name="shortcut"></param>
+        /// <returns></returns>
+        public static FileSystemInfo GetShortcutTarget(FileSystemInfo shortcut)
+        {
+            // Get the target path
+            IWshShell shell = new WshShell();
+            IWshShortcut lnk = shell.CreateShortcut(shortcut.FullName);
+            string target = lnk.TargetPath;
+
+            // Put it in a FileSystemInfo of the correct type
+            if (Directory.Exists(target))
+                return new DirectoryInfo(target);
+
+            return new FileInfo(target);
+        }
+
+        /// <summary>
+        /// Returns a shuffled version of the given list.
+        /// What'd you expect?
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        public static List<T> ShuffleList<T>(List<T> list)
+        {
+            Random randGen = new Random();
+            List<T> originalList = new List<T>();
+            List<T> outputList = new List<T>();
+
+            originalList.AddRange(list);
+            
+            while(originalList.Count > 0)
+            {
+                int index = randGen.Next(originalList.Count);
+                outputList.Add(originalList[index]);
+                originalList.RemoveAt(index);
+            }
+
+            return outputList;
+        }
+    }
+}
