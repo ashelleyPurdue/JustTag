9e504544fe13f933552d3c686a81f05b46adffe8
Alex Shelley <ashelley@purdue.edu>
2018-05-28 15:28:06 -0400

    Replaced video player in MainWindow with a FilePreviewer.

--- JustTag/FilePreviewer.xaml.cs
+++ JustTag/FilePreviewer.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace JustTag
 {
@@ -20,9 +21,34 @@ namespace JustTag
     /// </summary>
     public partial class FilePreviewer : UserControl
     {
+        public bool IsOpening { get; private set; }
+
         public FilePreviewer()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Shows a preview for the given file
+        /// </summary>
+        /// <param name="selectedFile"></param>
+        /// <returns></returns>
+        public async Task OpenPreview(FileInfo selectedFile)
+        {
+            // TODO: Preview different file types using different controls
+            IsOpening = true;
+            await videoPlayer.Open(selectedFile);
+            IsOpening = false;
+        }
+
+        /// <summary>
+        /// Closes the currently-open file preview
+        /// </summary>
+        /// <returns></returns>
+        public Task ClosePreview()
+        {
+            // TODO: Different closing behavior for different file types
+            return videoPlayer.UnloadVideo();
         }
     }
 }
