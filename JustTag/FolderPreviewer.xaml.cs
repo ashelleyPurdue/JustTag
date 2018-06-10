327202370d983cd8ec48597ba867c9473307ae52
Alex Shelley <ashelley@purdue.edu>
2018-06-09 22:15:45 -0400

    The icons are now more compact.

--- JustTag/FolderPreviewer.xaml.cs
+++ JustTag/FolderPreviewer.xaml.cs
@@ -22,25 +22,31 @@ namespace JustTag
     public partial class FolderPreviewer : UserControl
     {
         private const int MAX_ICONS = 5;
+        private const double IMAGE_HEIGHT = 100;
+        private const double SEPARATION_PERCENTAGE = 0.9;
+
         private List<Image> previewIcons = new List<Image>();
 
         public FolderPreviewer()
         {
             InitializeComponent();
-            const double IMAGE_HEIGHT = 100;
-            const double SEPARATION_PERCENTAGE = 0.9;
 
-            // Create all the icons
-            for (int i = 0; i < MAX_ICONS; i++)
-            {
-                Image image = new Image();
+            // Create the first icon
+            CreateIcon(0);
 
-                previewIcons.Add(image);
-                image.Margin = new Thickness(0, 0, 0, 0);
-                image.Height = IMAGE_HEIGHT;
+            // Create the rest of the icons
+            for (int i = 1; i < MAX_ICONS; i++)
+                CreateIcon(-previewIcons[i - 1].Height * SEPARATION_PERCENTAGE);
+        }
 
-                stackPanel.Children.Add(image);
-            }
+        private void CreateIcon(double offset)
+        {
+            Image image = new Image();
+            image.Margin = new Thickness(0, offset, 0, 0);
+            image.Height = IMAGE_HEIGHT;
+
+            previewIcons.Add(image);
+            stackPanel.Children.Add(image);
         }
 
         public async Task Open(DirectoryInfo dir)
