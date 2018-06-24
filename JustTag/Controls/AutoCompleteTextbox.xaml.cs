2aff51eb77d9793977e3e74dff3324fe3877d3c1
Alex Shelley <ashelley@purdue.edu>
2018-06-23 20:09:18 -0400

    Changed the namespaces of all controls in the Controls folder to JustTag.Controls

--- JustTag/Controls/AutoCompleteTextbox.xaml.cs
+++ JustTag/Controls/AutoCompleteTextbox.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace JustTag
+namespace JustTag.Controls
 {
     /// <summary>
     /// Interaction logic for AutoCompleteTextbox.xaml
--- JustTag/Controls/FileListItem.xaml.cs
+++ JustTag/Controls/FileListItem.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Shapes;
 
 using System.IO;
 
-namespace JustTag
+namespace JustTag.Controls
 {
     /// <summary>
     /// Interaction logic for FileListItem.xaml
