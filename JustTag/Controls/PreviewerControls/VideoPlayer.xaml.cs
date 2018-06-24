fb9a3a4f4468c3bab09c65f9c7ec47c79ba58874
Alex Shelley <ashelley@purdue.edu>
2018-06-23 20:14:53 -0400

    Fixed all errors resulting from the last two commits.

--- JustTag/Controls/PreviewerControls/VideoPlayer.xaml.cs
+++ JustTag/Controls/PreviewerControls/VideoPlayer.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using JustTag.Pages;
 
 namespace JustTag.Controls.PreviewerControls
 {
--- JustTag/Pages/FindReplaceTagsWindow.xaml.cs
+++ JustTag/Pages/FindReplaceTagsWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.IO;
+using JustTag.Controls;
 
 namespace JustTag.Pages
 {
