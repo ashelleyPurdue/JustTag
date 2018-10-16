c5966466d7553987acbaa931f186a4e65ba064f4
Alex Shelley <ashelley@purdue.edu>
2018-10-16 18:36:49 -0400

    Wrote trivial test case for normalizing(file name already normalized)

---
+++ TaggingTests/NormalizeTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JustTag.Tagging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TaggingTests
+{
+    [TestClass]
+    public class NormalizeTests
+    {
+        private static void AssertNormalized(string origName, string expectedName)
+        {
+            var orig = new TaggedFilePath(origName, false);
+            var normalized = orig.Normalize();
+
+            Assert.AreEqual<string>(expectedName, normalized.FullPath);
+        }
+
+        [TestMethod]
+        public void AlreadyNormalized() => AssertNormalized("foo[a b c].txt", "foo[a b c].txt");
+    }
+}
